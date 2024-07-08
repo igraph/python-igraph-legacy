b278cb69442514148f980da9c6d51b3924fafb8d
Tamas Nepusz <ntamas@gmail.com>
2024-07-09 01:37:36 +0200

    chore: bumped version to 0.11.6

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ if sys.version_info < (3, 6):
 
 from setuptools import setup
 
-VERSION = "0.11.5"
+VERSION = "0.11.6"
 
 description = """Python interface to the igraph high performance graph
 library, primarily aimed at complex network research and analysis.
